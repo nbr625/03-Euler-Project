c6c66d25348dfd4bcc19b4f180aed7f3c1563464
Nicolas Berrizbeitia <nbr625@gmail.com>
2015-07-28 08:42:47 -0400

    solved problem 2

--- fibonacci_evens.rb
+++ fibonacci_evens.rb
@@ -1,16 +1,36 @@
 
-fibo_even = []
-fibo_array = [1, 2]
 def fibonacci(array)
-
-	while array.length < 10
-		i = 0
-		next_num = array[i] + array[i + 1]
-		array.push(next_num)
+	upto = 4_000_000
+	i = 2
+	while array[i - 2] + array[i - 1] < upto
+		array << array[i - 2] + array[i - 1]
 		i += 1
 	end
-
-	puts array
+	return array
 end
-fibonacci(fibo_array)
+fibo = [1, 2]
+fibonacci(fibo)
 
+def even_sum(array)
+	sum_arr = []
+
+	array.each do |el|
+		if el % 2 == 0
+			sum_arr.push(el)
+		end
+	end
+	return sum_arr.inject(:+)
+end
+
+def glad_colors(string, color_code)
+  "\e[#{color_code}m#{string}\e[0m"
+end
+
+# Changes string to Blue
+def pink(string)
+  glad_colors(string, 35)
+end
+
+puts "Did you truly need to call me for this puny querry?"\
+ + pink("*sigh*\n") + "The answer is: " + pink(even_sum(fibo)) + \
+ ".\nNow allow me to " + pink("hate humanity") + " in peace"
