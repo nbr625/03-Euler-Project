adeeb4d42b05c861c87d3c867b03f5983aa03e81
Nicolas Berrizbeitia <nbr625@gmail.com>
2015-08-01 18:26:23 -0400

    solved 17

--- 15_gridlock.rb
+++ 15_gridlock.rb
@@ -1,18 +1,19 @@
 # 109287th place!
 require_relative 'glad_colors'
 
-# Update! I found a way to write the formula that I was trying to contruct initiallly. Today I learned about 
+# Update! I found a way to write the formula that I was initially trying to contruc. Today, I learned about 
 # The permuation method which is exactly what I required. My logic: The only directions are down and right.
-# The only thing that differs the route is when the right is made and when it is not. there fore if we
-# write a binary sequence of 0 and 1 and look for the permutations of twenty 0's and twenty 1's we arrive at the answer
+# The only thing that differs in each route is when the right is made and when it is not. there fore if we
+# write a binary sequence of 0 and 1 and look for the permutations of twenty 0's and twenty 1's we should arrive 
+# at the answer...
 
-# Formula: ([1, 0]*20).permutation.to_a.uniq.length     (Do not release from its hash prison.)
+# Formula: ([1, 0]*20).permutation.to_a.uniq.length     (Warning: Do not release from its hash prison.)
 
-# So simple but very sloooooow and dangerous (gladis could barely take it.) But the output is correct. 
-# I am just happy that my theory was right, no matter how harebrained it turned out to be as a solution.
+# So simple but very sloooooow and dangerous (gladis could barely take it.) The output is correct, though!
+# I am happy to know that my theory was right, no matter how harebrained it turned out as a solution.
 
 
-# Practical/boring solution
+# Practical/boring solution, based on a formula found during research.
 def factorize(num)
 	array = (1..num)
 	return array.inject(:*)
