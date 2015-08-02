ef190a3561441f28771029cd88330a22bb076ce2
Nicolas Berrizbeitia <nbr625@gmail.com>
2015-08-01 20:37:29 -0400

    refactored code to look more put together.

--- 17_numphalbet.rb
+++ 17_numphalbet.rb
@@ -1,28 +1,28 @@
 
 ## I am pretty proud of this one. Since I have not seen anyone use the product method to make derive the answer. 
 ## Aparently my bookworming throught he ruby docs is bearing fruit.
-
+require_relative 'glad_colors'
 
 hundreds = ["", "onehundredand", "twohundredand", "threehundredand", "fourhundredand", "fivehundredand", "sixhundredand", \
 "sevenhundredand", "eighthundredand", "ninehundredand" ]
 tens = ["", "", "twenty", "thirty", "forty", "fifty", "sixty", "seventy", "eighty", "ninety"]
 digits = ["", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine"]
-d_length =  digits.flatten.join("").length
+teenagers_thousand = (("teneleventwelvethirteenfourteenfifteensixteenseventeeneighteennineteen").length * 5) + 10
 
 
-alpha_array = hundreds.product(tens, digits)
+d_length = 
+
+alpha_num = 0
+alpha_array = 0
+
+def combinator(a, b, c)
+  alpha_array = a.product(b, c)
+  return alpha_array.flatten.join("").length - a.flatten.join("").length
+end
+
 	
 
-teenagers = ("teneleventwelvethirteenfourteenfifteensixteenseventeeneighteennineteen").length * 5 
-teenagers += 10	
-
-
-num = alpha_array.flatten.join("").length
-alpha_num = num - digits.flatten.join("").length
-
-
-puts teenagers + alpha_num
-
+puts "Your answer: " + pink(teenagers_thousand + combinator(digits, tens, hundreds)) + " aren't we a clever " + pink("primate.")
 
 
 
