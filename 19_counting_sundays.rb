b4d6f6793d78a039961d11f64977e0f7e57cefd0
Nicolas Berrizbeitia <nbr625@gmail.com>
2015-08-03 00:30:34 -0400

    fixed up counting 19 and added glad colors

--- 19_counting_sundays.rb
+++ 19_counting_sundays.rb
@@ -1,48 +1,47 @@
-
-d = 1 #day
-m = 1 #month
-y = 1900 #year
-wd = 2 #weekday
-
-thirtydays = [4, 6, 9, 11] # months with thirty days
-s = 0 #number of sundays
-
-def leap?(year)
-	true if year % 400 == 0
-	false if year if y % 100 == 0
+require_relative 'glad_colors'
+thirty = [4, 6, 9, 11]
+ 
+m = 1 
+d = 1 
+y = 1900
+dw = 2
+ 
+def leap? y
+  return true if y % 400 == 0
+  return false if y % 100 == 0 
+  y % 4 == 0   
 end
-
-
-
+ 
+sundays = 0
+ 
 while(y < 2001)
-	
-	if d == 1 && wd == 1 && y > 1900
-		s += 1
-	end
-
-	wd += 1
-	wd = 1 if wd  == 8
-
-	case
-	when 1..27
-		d += 1
-	when 28 && m == 2 && leap?(y)
-		d, m = 1, 3
-	when 28 && m == 2 && !leap?(y)
-		d += 1
-	when 29 && m == 2
-		d, m = 1, 3
-	when 29
-		d += 1
-	when 30 && thirtydays.include?
-		d, m = 1, d + 1 
-	when 30
-		d += 1
-	when 31
-		d, m = 1, m + 1
-		m, y = 1, y + 1 if m == 13
-	end
-end
-
-puts s
-
+   
+   sundays += 1 if (dw == 1 and d == 1 and y > 1900)
+    
+   dw += 1
+   dw = 1 if dw == 8
+       
+   case d
+    when 1..27
+      d += 1
+    when 28 && m == 2 && leap?(y)     
+      d += 1
+    when 28 && m == 2 && !leap?(y) 
+      d, m = 1, 3       
+    when 28
+      d += 1     
+    when 29 && m == 2
+      d, m = 1, 3
+    when 29
+      d += 1     
+    when 30 && thirty.include?(m)
+      d, m = 1, d + 1     
+    when 30
+      d += 1     
+    when 31
+      d, m = 1, m + 1
+      m, y = 1, y + 1 if m == 13
+   end  
+end 
+ 
+puts pink(sundays) + " is the answer. " + pink("dim") + " is you."
